36d54fbe5b0adfde82a64d7ed20c6a37e4503884
Vit <hutki@tut.by>
2017-05-04 10:25:20 +0300

    file add

---
+++ filtr_snippet.php
@@ -0,0 +1,234 @@
+<?php
+if (isset($_GET['gender']))
+    $gender = $_GET['gender'];
+
+if (isset($_GET['face']))
+    $face = $_GET['face'];
+
+if (isset($_GET['hair']))
+    $hair = $_GET['hair'];
+
+if (isset($_GET['l_hair']))
+    $l_hair = $_GET['l_hair'];
+
+if (isset($_GET['eyes']))
+    $eyes = $_GET['eyes'];
+
+if (isset($_GET['age1']))
+    $age1 = $_GET['age1'];
+
+
+if (isset($_GET['age2']))
+    $age2 = $_GET['age2'];
+
+if (isset($_GET['growth1']))
+    $growth1 = $_GET['growth1'];
+
+if (isset($_GET['growth2']))
+    $growth2 = $_GET['growth2'];
+
+if (isset($_GET['chest1']))
+    $chest1 = $_GET['chest1'];
+
+if (isset($_GET['chest2']))
+    $chest2 = $_GET['chest2'];
+
+if (isset($_GET['waist1']))
+    $waist1 = $_GET['waist1'];
+
+if (isset($_GET['waist2']))
+    $waist2 = $_GET['waist2'];
+
+if (isset($_GET['hip1']))
+    $hip1 = $_GET['hip1'];
+
+if (isset($_GET['hip2']))
+    $hip2 = $_GET['hip2'];
+
+if (isset($_GET['breast_size']))
+    $breast_size = $_GET['breast_size'];
+
+if (isset($_GET['clothing1']))
+    $clothing1 = $_GET['clothing1'];
+
+if (isset($_GET['clothing2']))
+    $clothing2 = $_GET['clothing2'];
+
+if (isset($_GET['shoes1']))
+    $shoes1 = $_GET['shoes1'];
+
+if (isset($_GET['shoes2']))
+    $shoes2 = $_GET['shoes2'];
+
+if (isset($_GET['work']))
+    $work = $_GET['work'];
+
+if (isset($_GET['abroad']))
+    $abroad = $_GET['abroad'];
+
+if (isset($_GET['language']))
+    $language = $_GET['language'];
+
+if (isset($_GET['expert']))
+    $expert = $_GET['expert'];
+
+if (isset($_GET['client']))
+    $client = $_GET['client'];
+
+if (isset($_GET['clean_p']))
+    $clean_p = $_GET['clean_p'];
+
+//----------------------------------------------------------------------------
+
+$docs_array = array();
+
+$sql = "SELECT tmplvarid,contentid,value FROM modx_site_tmplvar_contentvalues";
+$result = $modx->query($sql);
+
+// Подготавливаем данные
+while ($data = $result->fetch(PDO::FETCH_ASSOC))
+{
+switch ($data['tmplvarid'])
+{
+case 34: // Id tv параметра 'Пол'
+$docs_array[$data['contentid']]['gender'] = $data['value'];
+break;
+case 13: // Id tv параметра 'Тип лица'
+$docs_array[$data['contentid']]['face'] = $data['value'];
+break;
+case 31: // Id tv параметра 'Цвет волос'
+$docs_array[$data['contentid']]['hair'] = $data['value'];
+break;
+case 32: // Id tv параметра 'Длина волос'
+$docs_array[$data['contentid']]['l_hair'] = $data['value'];
+break;
+case 33: // Id tv параметра 'Цвет глаз'
+$docs_array[$data['contentid']]['eyes'] = $data['value'];
+break;
+case 9: // Id tv параметра 'Год рождения'
+$docs_array[$data['contentid']]['age1'] = abs((string)(round((strtotime($data['value'])-strtotime('Y'))/31536000)));
+break;
+case 1: // Id tv параметра 'Рост' 
+$docs_array[$data['contentid']]['growth1'] = $data['value'];
+break;
+case 2: // Id tv параметра 'Грудь' 
+$docs_array[$data['contentid']]['chest1'] = $data['value'];
+break;
+case 3: // Id tv параметра 'Талия' 
+$docs_array[$data['contentid']]['waist1'] = $data['value'];
+break;
+case 4: // Id tv параметра 'Бедра' 
+$docs_array[$data['contentid']]['hip1'] = $data['value'];
+break;
+case 11: // Id tv параметра 'Размер груди' 
+$docs_array[$data['contentid']]['breast_size'] = $data['value'];
+break;
+case 5: // Id tv параметра 'Размер одежды' 
+$docs_array[$data['contentid']]['clothing1'] = $data['value'];
+break;
+case 12: // Id tv параметра 'Размер обуви' 
+$docs_array[$data['contentid']]['shoes1'] = $data['value'];
+break;
+case 14: // Id tv параметра 'work' 
+$docs_array[$data['contentid']]['work'] = $data['value'];
+break;
+case 18: // Id tv параметра 'abroad' 
+$docs_array[$data['contentid']]['abroad'] = $data['value'];
+break;
+case 15: // Id tv параметра 'language' !!!!!!!!!!!!!!!!!!!!!не workет
+$docs_array[$data['contentid']]['language'] = $data['value'];
+break;
+case 19: // Id tv параметра 'expert' 
+$docs_array[$data['contentid']]['expert'] = $data['value'];
+break;
+case 20: // Id tv параметра 'expert' 
+$docs_array[$data['contentid']]['client'] = $data['value'];
+break;
+
+}
+}
+ // Теперь у нас каждый документ имеет полный набор tv параметров!!!!
+ // [10] => Array ( [face] => 175 [gender] => 3 ) [16] => Array ( [face] => 175 [gender] => 3 )
+ // где 10 и 16 id доукмента
+
+ // Выполняем выборку в соответствии с фильтрами
+    $docs_ids = '';
+ foreach ($docs_array as $key => $val)
+ {
+ if (
+  //пол
+  ($val['gender'] == $gender || !isset($val['gender'])) &&
+  //тип лица
+  ($val['face'] == $face || !isset($val['face']) || empty($face) || !isset($face)) &&
+  //цвет волос
+  ($val['hair'] == $hair || !isset($val['hair']) || empty($hair) || !isset($hair)) &&
+  //длина волос
+  ($val['l_hair'] == $l_hair || !isset($val['l_hair']) || empty($l_hair) || !isset($l_hair)) &&
+  //цвет глаз
+  ($val['eyes'] == $eyes || !isset($val['eyes']) || empty($eyes) || !isset($eyes)) &&
+  //возраст
+   (((empty($age1) || !isset($age1)) && ($val['age1'] >= 0 && $val['age1'] <= $age2)) ||
+   ((empty($age2) || !isset($age2)) && $val['age1'] >= $age1)||
+   ((empty($age2) || !isset($age2)) && (empty($age1) || !isset($age1)))||
+   ($val['age1'] >= $age1 && $val['age1'] <= $age2)) && 
+   //рост
+  (((empty($growth1) || !isset($growth1)) && ($val['growth1'] >= 0 && $val['growth1'] <= $growth2)) ||
+   ((empty($growth2) || !isset($growth2)) && $val['growth1'] >= $growth1)||
+   ((empty($growth2) || !isset($growth2)) && (empty($growth1) || !isset($growth1)))||
+   ($val['growth1'] >= $growth1 && $val['growth1'] <= $growth2)) && 
+  //грудь
+  (((empty($chest1) || !isset($chest1)) && ($val['chest1'] >= 0 && $val['chest1'] <= $chest2)) ||
+   ((empty($chest2) || !isset($chest2)) && $val['chest1'] >= $chest1)||
+   ((empty($chest2) || !isset($chest2)) && (empty($chest1) || !isset($chest1)))||
+   ($val['chest1'] >= $chest1 && $val['chest1'] <= $chest2)) &&
+  //талия
+  (((empty($waist1) || !isset($waist1)) && ($val['waist1'] >= 0 && $val['waist1'] <= $waist2)) ||
+     ((empty($waist2) || !isset($waist2)) && $val['waist1'] >= $waist1)||
+     ((empty($waist2) || !isset($waist2)) && (empty($waist1) || !isset($waist1)))||
+     ($val['waist1'] >= $waist1 && $val['waist1'] <= $waist2)) &&
+  //бедра
+  (((empty($hip1) || !isset($hip1)) && ($val['hip1'] >= 0 && $val['hip1'] <= $hip2)) ||
+     ((empty($hip2) || !isset($hip2)) && $val['hip1'] >= $hip1)||
+     ((empty($hip2) || !isset($hip2)) && (empty($hip1) || !isset($hip1)))||
+     ($val['hip1'] >= $hip1 && $val['hip1'] <= $hip2)) &&
+  //размер груди
+   ($val['breast_size'] == $breast_size || !isset($val['breast_size']) || empty($breast_size) || !isset($breast_size)) &&
+  //размер одежды
+  (((empty($clothing1) || !isset($clothing1)) && ($val['clothing1'] >= 0 && $val['clothing1'] <= $clothing2)) ||
+     ((empty($clothing2) || !isset($clothing2)) && $val['clothing1'] >= $clothing1)||
+     ((empty($clothing2) || !isset($clothing2)) && (empty($clothing1) || !isset($clothing1)))||
+     ($val['clothing1'] >= $clothing1 && $val['clothing1'] <= $clothing2)) &&
+  //размер обуви
+  (((empty($shoes1) || !isset($shoes1)) && ($val['shoes1'] >= 0 && $val['shoes1'] <= $shoes2)) ||
+   ((empty($shoes2) || !isset($shoes2)) && $val['shoes1'] >= $shoes1)||
+   ((empty($shoes2) || !isset($shoes2)) && (empty($shoes1) || !isset($shoes1)))||
+   ($val['shoes1'] >= $shoes1 && $val['shoes1'] <= $shoes2)) &&
+  //опыт работы
+   ($val['work'] == $work || !isset($val['work']) || empty($work) || !isset($work)) &&
+   //гастробайтер
+   ($val['abroad'] == $abroad || !isset($val['abroad']) || empty($abroad) || !isset($abroad)) &&
+   //языки
+    ((preg_match('/^(.)*'.$language.'(.)*$/uis', $val['language']))  || empty($language) || !isset($language)) &&
+  //эксперт
+    ($val['expert'] == $expert || !isset($val['expert']) || empty($expert) || !isset($expert)) &&
+  //клиент
+     ($val['client'] == $client || !isset($val['client']) || empty($client) || !isset($client)) &&
+     (!isset($val['language']) == $clean_p || empty($clean_p) || !isset($clean_p))
+     //анкеты с пустыми полями
+ 
+
+
+    ) // Если соответствует двум параметрам сразу
+
+         $docs_ids .=  $key.',';
+
+
+
+
+ }
+ // Удаляем в конце запятую
+ $docs_ids = substr($docs_ids, 0, -1);
+
+ // Пихаем в дитто и выводим!!!
+
+return $docs_ids;
---
+++ form_snippet.php
@@ -0,0 +1,249 @@
+<?php
+// Начальное значение
+
+$face_options = '';
+$hair_options = '';
+
+
+
+// Получаем значения с GET
+$get_gender = (isset($_GET['gender']))?$_GET['gender']:'';
+
+if($get_gender == 'Муж'){
+	$id_gender = 'active_gender_man';
+}else{
+	$id_gender1 = 'active_gender_woman';
+}
+
+
+$get_face = (isset($_GET['face']))?$_GET['face']:'';
+$get_hair = (isset($_GET['hair']))?$_GET['hair']:'';
+$get_l_hair = (isset($_GET['l_hair']))?$_GET['l_hair']:'';
+$get_eyes = (isset($_GET['eyes']))?$_GET['eyes']:'';
+$get_age1 = (isset($_GET['age1']))?$_GET['age1']:'';
+$get_growth1 = (isset($_GET['growth1']))?$_GET['growth1']:'';
+$get_growth2 = (isset($_GET['growth2']))?$_GET['growth2']:'';
+$get_chest1 = (isset($_GET['chest1']))?$_GET['chest1']:'';
+$get_chest2 = (isset($_GET['chest2']))?$_GET['chest2']:'';
+$get_waist1 = (isset($_GET['waist1']))?$_GET['waist1']:'';
+$get_waist2 = (isset($_GET['waist2']))?$_GET['waist2']:'';
+$get_hip1 = (isset($_GET['hip1']))?$_GET['hip1']:'';
+$get_hip2 = (isset($_GET['hip2']))?$_GET['hip2']:'';
+$get_breast_size = (isset($_GET['breast_size']))?$_GET['breast_size']:'';
+$get_clothing1 = (isset($_GET['clothing1']))?$_GET['clothing1']:'';
+$get_clothing2 = (isset($_GET['clothing2']))?$_GET['clothing2']:'';
+$get_shoes1 = (isset($_GET['shoes1']))?$_GET['shoes1']:'';
+$get_shoes2 = (isset($_GET['shoes2']))?$_GET['shoes2']:'';
+$get_work = (isset($_GET['work']))?$_GET['work']:'';
+$get_language = (isset($_GET['language']))?$_GET['language']:'';
+$get_abroad = (isset($_GET['abroad']))?$_GET['abroad']:'';
+$get_expert = (isset($_GET['expert']))?$_GET['expert']:'';
+$get_client = (isset($_GET['client']))?$_GET['client']:'';
+
+if(!empty($_GET['face']))
+	$sel = 'style"background:red;"';
+
+
+// Формируем данные вывода
+
+
+$face_array = array(	array('option' => '', 'val' => 'Тип лица'),
+						array('option' => 'европейская', 'val' => 'европейский'),
+						array('option' => 'азиатская', 'val' => 'азиатский'),
+						array('option' => 'мулат', 'val' => 'мулат'),
+						array('option' => 'афро', 'val' => 'африканский'),
+						array('option' => 'метис', 'val' => 'метис'));
+$hair_array = array(	array('option' => '', 'val' => 'Цвет волос'),
+						array('option' => 'Блондин(ка', 'val' => 'светлый'),
+						array('option' => 'русый', 'val' => 'русый'),
+						array('option' => 'Рыжий(ая)', 'val' => 'рыжий'),
+						array('option' => 'Шатен(ка)', 'val' => 'каштановый'),
+						array('option' => 'Брюнет(ка)', 'val' => 'черный'));
+$l_hair_array = array(	array('option' => '', 'val' => 'Длина волос'),
+						array('option' => 'Длиные', 'val' => 'длинные'),
+						array('option' => 'Средние', 'val' => 'средние'),
+						array('option' => 'Короткие', 'val' => 'короткие'),
+						array('option' => 'Шатен(ка)', 'val' => 'каштановый'),
+						array('option' => 'Без волос', 'val' => 'отсутствуют'));
+
+$eyes_array = array(	array('option' => '', 'val' => 'Цвет глаз'),
+						array('option' => 'Карие', 'val' => 'карие'),
+						array('option' => 'Зеленые', 'val' => 'зеленые'),
+						array('option' => 'Голубые', 'val' => 'голубые'),
+						array('option' => 'Серые', 'val' => 'серые'));
+
+$breast_size_array = array(	array('option' => '', 'val' => 'Размер груди'),
+						array('option' => '0', 'val' => '0'),
+						array('option' => '0,5', 'val' => '0,5'),
+						array('option' => '1', 'val' => '1'),
+						array('option' => '1,5', 'val' => '1,5'),
+						array('option' => '2', 'val' => '2'),
+						array('option' => '2,5', 'val' => '2,5'),
+						array('option' => '3', 'val' => '3'),
+						array('option' => '3,5', 'val' => '3,5'),
+						array('option' => '4', 'val' => '4'),
+						array('option' => '4,5', 'val' => '4,5'),
+						array('option' => '5', 'val' => '5'),
+						array('option' => '5,5', 'val' => '5,5'),
+						array('option' => 'больше 5', 'val' => 'больше 5'));
+
+$work_array = array(	array('option' => '', 'val' => 'Опыт работы'),
+						array('option' => 'менее года', 'val' => 'без опыта'),
+						array('option' => '1-3 года', 'val' => 'небольшой'),
+						array('option' => 'более 3-х лет', 'val' => 'большой'));
+				
+
+$language_array = array(	array('option' => '', 'val' => 'Знание языков'),
+						array('option' => 'английский', 'val' => 'английский'),
+						array('option' => 'немецкий', 'val' => 'немецкий'),
+						array('option' => 'французский', 'val' => 'французский'),
+						array('option' => 'итальянский', 'val' => 'итальянский'),
+						array('option' => 'испанский', 'val' => 'испанский'),
+						array('option' => 'китайский', 'val' => 'китайский'),
+						array('option' => 'японский', 'val' => 'японский'));
+
+$abroad_array = array(	array('option' => '', 'val' => 'Готовность работать за границей'),
+						array('option' => 'Готова к работе за рубежом', 'val' => 'да'),
+						array('option' => 'Не готова к работе за рубежом', 'val' => 'нет'));
+
+
+
+// Формируем разметку для селекта face
+foreach ($face_array as $val)
+	$face_options .= '<option value="'.$val['option'].'" '.(($val['option'] == $get_face)?'selected':'').'>'.$val['val'].'</option>';
+foreach ($hair_array as $val)
+	$hair_options .= '<option value="'.$val['option'].'" '.(($val['option'] == $get_hair)?'selected':'').'>'.$val['val'].'</option>';
+foreach ($l_hair_array as $val)
+	$l_hair_options .= '<option value="'.$val['option'].'" '.(($val['option'] == $get_l_hair)?'selected':'').'>'.$val['val'].'</option>';
+foreach ($eyes_array as $val)
+	$eyes_array_options .= '<option value="'.$val['option'].'" '.(($val['option'] == $get_eyes)?'selected':'').'>'.$val['val'].'</option>';
+foreach ($breast_size_array as $val)
+	$breast_size_array_options .= '<option value="'.$val['option'].'" '.(($val['option'] == $get_breast_size)?'selected':'').'>'.$val['val'].'</option>';
+foreach ($work_array as $val)
+	$work_array_options .= '<option value="'.$val['option'].'" '.(($val['option'] == $get_work)?'selected':'').'>'.$val['val'].'</option>';
+foreach ($language_array as $val)
+	$language_array_options .= '<option value="'.$val['option'].'" '.(($val['option'] == $get_language)?'selected':'').'>'.$val['val'].'</option>';
+foreach ($abroad_array as $val)
+	$abroad_array_options .= '<option value="'.$val['option'].'" '.(($val['option'] == $get_abroad)?'selected':'').'>'.$val['val'].'</option>';
+// формируем форму
+
+
+$result = '<div class="l_block">
+<form action="" method="get" name="model">
+<select name="services">
+	<option>Вид работы</option>
+	<option value="услуги">наши услуги</option>
+</select>
+<div class="gender_block">
+	<label id="'.$id_gender.'" class="gender" for="men"><input type="radio" name="gender" id="men" value="Муж">Мужчина</label><span></span>
+	<label  id="'.$id_gender1.'" class="gender" for="women"><input type="radio"  checked="checked" name="gender" id="women" value="Жен">Женщина</label>
+</div>
+<select id="select1" name="face" '.$sel.'>
+	'.$face_options.'
+</select>
+<select name="hair">
+	'.$hair_options.'
+</select>
+<select name="l_hair">
+	'.$l_hair_options.'
+</select>
+<select name="eyes">
+	'.$eyes_array_options.'
+</select>
+<div class="block_inp">
+	<span>Возраст: </span>
+	<div class="range_inp">
+		<input name="age1" type="number" placeholder="от '.$get_age1.'"> - 
+		<input name="age2" type="number" placeholder="до  '.$get_age2.'">
+	</div>
+</div>
+<div class="block_inp">
+	<span>Рост: </span>
+	<div class="range_inp">
+		<input name="growth1" type="number" placeholder="от '.$get_growth1.'"> - 
+		<input name="growth2" type="number" placeholder="до  '.$get_growth2.'">
+	</div>
+</div>
+<div class="block_inp">
+	<span>Грудь: </span>
+	<div class="range_inp">
+		<input name="chest1" type="number" placeholder="от '.$get_chest1.'"> - 
+		<input name="chest2" type="number" placeholder="до '.$get_chest1.'">
+	</div>
+</div>
+<div class="block_inp">
+	<span>Талия: </span>
+	<div class="range_inp">
+		<input name="waist1" type="number" placeholder="от '.$get_waist1.'"> - 
+		<input name="waist2" type="number" placeholder="до '.$get_waist1.'">
+	</div>
+</div>
+<div class="block_inp">
+	<span>Бедра: </span>
+	<div class="range_inp">
+		<input name="hip1" type="number" placeholder="от  '.$get_hip1.'"> - 
+		<input name="hip2" type="number" placeholder="до  '.$get_hip2.'">
+	</div>
+</div>
+<div class="block_inp">
+	<span>Вес: </span>
+	<div class="range_inp">
+		<input name="weight1" type="number" placeholder="от"> - 
+		<input name="weight2" type="number" placeholder="до">
+	</div>
+</div>
+<select name="breast_size">
+	'.$breast_size_array_options.'
+</select>
+<div class="block_inp dress">
+	<span>Размер одежды: </span>
+	<div class="range_inp">
+		<input name="clothing1" type="number" placeholder="от '.$get_clothing1.'"> - 
+		<input name="clothing2" type="number" placeholder="до '.$get_clothing1.'">
+	</div>
+</div>
+<div class="block_inp dress">
+	<span>Размер обуви: </span>
+	<div class="range_inp">
+		<input name="shoes1" type="number" placeholder="от '.$get_shoes1.'"> - 
+		<input name="shoes2" type="number" placeholder="до '.$get_shoes2.'">
+	</div>
+</div>
+<select name="work">
+	'.$work_array_options.'
+</select>
+<select name="language">
+	'.$language_array_options.'
+</select>
+<select name="abroad">
+	'.$abroad_array_options.'
+</select>
+<div class="dop_opt">
+	<div class="clean_inp">
+		<input  type="radio" name="clean_p" id="clean_p" value="пустые_поля"><label for="clean_p">Анкеты с пустыми<br>(незаполненными) полями</label>
+	</div>
+	<div class="exp_inp">
+		<span>Выбор по рейтингу эксперта: </span>
+		<div class="range_inp">
+			<input name="expert" type="text" placeholder="1 - 10 '.$get_expert.'">
+		</div>
+	</div>
+	<div class="exp_inp">
+		<span>Выбор по рейтингу клиента: </span>
+		<div class="range_inp">
+			<input name="client" type="text" placeholder="1 - 10 '.$get_client.'">
+		</div>
+	</div>
+</div>
+<div class="butt_block">
+	<button id="pokazat" type="submit">Показать</button>
+	<a href="[[~30]]" id="reset">Очистить все поля</a>
+
+
+</div>
+
+</form>
+</div>';
+
+
+print $result;
